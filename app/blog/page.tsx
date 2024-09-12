869270c534bd187b49e719f3a500e372bd6db059
Aleksan <sanyok.10rus@gmail.com>
2024-09-12 17:46:00 +0300

    added metadata for pages

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -1,11 +1,12 @@
-import Head from "next/head";
+import { Metadata } from "next";
 
+export const metadata: Metadata = {
+  title: "Blog | Next App",
+  description: "blog page",
+};
 const Blog = () => {
   return (
     <div className="page">
-      <Head>
-        <title>Blog</title>
-      </Head>
       <h1>Blog Page</h1>
     </div>
   );
