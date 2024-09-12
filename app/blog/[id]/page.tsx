a37a26c79d4963b8bf0dc23bbcb51d72b7a6a652
Aleksan <sanyok.10rus@gmail.com>
2024-09-12 18:28:35 +0300

    added pages for error, loader and method to get posts on pages

--- app/blog/[id]/page.tsx
+++ app/blog/[id]/page.tsx
@@ -1,5 +1,19 @@
 import { Metadata } from "next";
 
+const getData = async (id: string) => {
+  const response = await fetch(`https://jsonplaceholder.typicode.com/posts/${id}`, {
+    next: {
+      revalidate: 60
+    }
+  });
+  return response.json();
+}
+
+interface IPost {
+  title: string;
+  body: string;
+}
+
 type Props = {
   params: { id: string };
 }
@@ -12,12 +26,15 @@ export const generateMetadata = ({ params }: Props): Metadata => {
   };
 };
 
-const Post = ({ params }: Props) => {
+const Post = async ({ params }: Props) => {
   const { id } = params;
+  const post: IPost = await getData(id);
+  console.log(post);
 
   return (
     <div className="page">
-      <div>Post page {id}</div>
+      <h1>{post.title}</h1>
+      <p>{post.body}</p>
     </div>
   );
 };
